dd97e796bae8ee90fddb1274ed0b34724be28dc0
Darius Roberts <darius.roberts@gmail.com>
2015-12-10 22:27:43 -0800

    allow revision param (some Package repos abuse branches)

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,16 +2,18 @@
 #
 # namevar - The name of the package.
 # source  - The location of the git repository containing the package.
+# revision - The ref, branch or tag containing the package.
 #
 # Examples
 #
 #   sublime_text::package { 'Emmet':
 #     source => 'sergeche/emmet-sublime'
 #   }
-define sublime_text::package($source) {
+define sublime_text::package($source='',$revision='master') {
   require sublime_text::config
 
   repository { "${sublime_text::config::packagedir}/${name}":
-    source => $source
+    source => $source,
+    revision => $revision
   }
 }
